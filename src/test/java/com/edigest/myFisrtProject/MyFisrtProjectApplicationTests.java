7cfe481bd2cf36b79793823f33be78ed0abc6edd
Haroon Asif <haroon.asif@tkxel.com>
2025-01-05 00:57:12 +0500

    Initial commit

---
+++ src/test/java/com/edigest/myFisrtProject/MyFisrtProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edigest.myFisrtProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFisrtProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
